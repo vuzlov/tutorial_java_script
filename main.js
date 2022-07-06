3b40934942126591d080e8580e9c176c3b7a701d
женя вузлов <evuzlov@gmail.com.com>
2022-07-07 02:02:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let count= 7;
+console.log('значение:',count);
+count=7*7;
+console.log('квадрат этого зачения:', count);
+count=7*7*7;
+console.log('куб этого значения:', count);
+
---
+++ second.js
@@ -0,0 +1,6 @@
+let cels= 40;
+let faring= (cels*1.8)+32;
+console.log (cels+'\u00B0'+'C', 'равно', faring+'\u00B0'+'F');
+let faringeut= 104;
+let celsiu= (faringeut-32)*5/9;
+console.log(faringeut+'\u00B0'+'F', "равно", celsiu+'\u00B0'+'C');
